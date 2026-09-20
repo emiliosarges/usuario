050cfbde3f10aeb8e52da978fd1566197a34d2df
emiliosarges <emiliosarges@gmail.com>
2026-09-19 23:34:08 -0300

    adicionadas classes de security

---
+++ src/main/java/com/souemilio/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.souemilio.aprendendospring.infrastructure.repository;
+
+import com.souemilio.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ src/main/java/com/souemilio/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.souemilio.aprendendospring.infrastructure.repository;
+
+import com.souemilio.aprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
